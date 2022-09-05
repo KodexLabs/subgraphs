788dde7336d0f753d6160104f349f15a4786a616
p7m <perpetuum7@protonmail.com>
2022-09-05 21:25:04 +0200

    refactor: eslint

--- subgraphs/seaport/tests/mocks.ts
+++ subgraphs/seaport/tests/mocks.ts
@@ -1,41 +1,27 @@
-import { Address, Bytes, ethereum } from "@graphprotocol/graph-ts";
-import { newMockEvent } from "matchstick-as";
-import { OrderValidated as OrderValidatedEvent } from "../generated/Seaport/Seaport";
+import { Address, Bytes, ethereum } from '@graphprotocol/graph-ts';
+import { newMockEvent } from 'matchstick-as';
+import { OrderValidated as OrderValidatedEvent } from '../generated/Seaport/Seaport';
 
-export function createOrderValidatedEvent(
-    validator: Address,
-    orderHash: Bytes,
-    offerer: Address,
-    zone: Address
-): OrderValidatedEvent {
-    const mockEvent = newMockEvent();
-    const event = new OrderValidatedEvent(
-        validator,
-        mockEvent.logIndex,
-        mockEvent.transactionLogIndex,
-        mockEvent.logType,
-        mockEvent.block,
-        mockEvent.transaction,
-        mockEvent.parameters,
-        mockEvent.receipt
-    );
-    event.parameters = new Array();
+export function createOrderValidatedEvent(validator: Address, orderHash: Bytes, offerer: Address, zone: Address): OrderValidatedEvent {
+	const mockEvent = newMockEvent();
+	const event = new OrderValidatedEvent(
+		validator,
+		mockEvent.logIndex,
+		mockEvent.transactionLogIndex,
+		mockEvent.logType,
+		mockEvent.block,
+		mockEvent.transaction,
+		mockEvent.parameters,
+		mockEvent.receipt
+	);
+	event.parameters = [];
 
-    const orderHashParam = new ethereum.EventParam(
-        "orderHash",
-        ethereum.Value.fromBytes(orderHash)
-    );
-    const offererParam = new ethereum.EventParam(
-        "offerer",
-        ethereum.Value.fromAddress(offerer)
-    );
-    const zoneParam = new ethereum.EventParam(
-        "zone",
-        ethereum.Value.fromAddress(zone)
-    );
-    event.parameters.push(orderHashParam);
-    event.parameters.push(offererParam);
-    event.parameters.push(zoneParam);
+	const orderHashParam = new ethereum.EventParam('orderHash', ethereum.Value.fromBytes(orderHash));
+	const offererParam = new ethereum.EventParam('offerer', ethereum.Value.fromAddress(offerer));
+	const zoneParam = new ethereum.EventParam('zone', ethereum.Value.fromAddress(zone));
+	event.parameters.push(orderHashParam);
+	event.parameters.push(offererParam);
+	event.parameters.push(zoneParam);
 
-    return event;
+	return event;
 }
