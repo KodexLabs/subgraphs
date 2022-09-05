dc3035b250850de7f341bd03bddf2cdd923cd31e
p7m <perpetuum7@protonmail.com>
2022-09-05 22:18:38 +0200

    feat: ens subgraph

--- config/mainnet.js
+++ config/mainnet.js
@@ -1,9 +1,21 @@
 module.exports = {
 	network: 'mainnet',
 	ens: {
+		ensRegistryOld: {
+			address: '0x314159265dd8dbb310642f98f50c066173c1259b',
+			startBlock: 3327417
+		},
+		ensRegistry: {
+			address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+			startBlock: 9380380
+		},
 		baseRegistrar: {
 			address: '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85',
 			startBlock: 9380410
+		},
+		ethRegistrarController: {
+			address: '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5',
+			startBlock: 9380471
 		}
 	},
 	opensea: {
